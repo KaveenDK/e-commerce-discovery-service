5016c25fc34049562b914b8ec4c85b33f23424aa
KaveeN <vpdkkaveen@gmail.com>
2026-02-06 10:42:18 +0530

    add InventoryController with health-check endpoint and update IntelliJ and application.properties

---
+++ inventory-service/src/main/java/lk/ijse/edu/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,29 @@
+package lk.ijse.edu.inventoryservice.controller;
+
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * --------------------------------------------
+ * @Author Dimantha Kaveen
+ * @GitHub: https://github.com/KaveenDK
+ * @Website: https://dimantha-kaveen-portfolio.web.app/
+ * @Linkdin: https://www.linkedin.com/in/KaveeNDK/
+ * --------------------------------------------
+ * @Created 2/6/2026
+ * @Project e-commerce
+ * --------------------------------------------
+ **/
+
+@RestController
+@RequestMapping("/api/v1/inventory")
+public class InventoryController {
+
+    @GetMapping("/helth")
+    public ResponseEntity<String> checkHelth(){
+        return new ResponseEntity<>("Good", HttpStatusCode.valueOf(200));
+    }
+}
