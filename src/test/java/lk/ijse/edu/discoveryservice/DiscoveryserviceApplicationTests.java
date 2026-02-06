75b9a09d2c978d81b0a0cba4d9063dac2129bf59
KaveeN <vpdkkaveen@gmail.com>
2026-02-06 09:48:57 +0530

    first commit

---
+++ src/test/java/lk/ijse/edu/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.edu.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
