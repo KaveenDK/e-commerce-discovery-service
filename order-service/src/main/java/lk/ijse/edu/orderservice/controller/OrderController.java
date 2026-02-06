82067d9323db64052cd3929bf72fb5c7fe1c210d
KaveeN <vpdkkaveen@gmail.com>
2026-02-06 11:01:01 +0530

    add OrderController with health-check endpoint, integrate order-service with Eureka Discovery Client, and update IntelliJ project files

---
+++ order-service/src/main/java/lk/ijse/edu/orderservice/controller/OrderController.java
@@ -0,0 +1,29 @@
+package lk.ijse.edu.orderservice.controller;
+
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * --------------------------------------------
+ * @Author Dimantha Kaveen
+ * @GitHub: https://github.com/KaveenDK
+ * @Website: https://dimantha-kaveen-portfolio.web.app/
+ * @Linkdin: https://www.linkedin.com/in/KaveeNDK/
+ * --------------------------------------------
+ * @Created 2/6/2026
+ * @Project e-commerce
+ * --------------------------------------------
+ **/
+
+@RestController
+@RequestMapping("/api/v1/order")
+public class OrderController {
+
+    @GetMapping("/helth")
+    public ResponseEntity<String> checkHelth(){
+        return new ResponseEntity<>("Good", HttpStatusCode.valueOf(200));
+    }
+}
